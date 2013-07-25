746ab396538fd86c8026930557c23a6751b7bfab
Shady <shady@shadyproject.net>
2013-07-25 00:55:59 -0700

    solution for compute prediction

---
+++ prediction.rb
@@ -0,0 +1,10 @@
+def compute_prediction(n, w)
+  growth_rate = 0.07
+  prediction = (1..w).inject(n) {|sum, _| sum + (sum *growth_rate)}
+  puts prediction.floor
+end
+
+uniq_per_week = 10
+weeks = 3
+
+compute_prediction(uniq_per_week, weeks)
