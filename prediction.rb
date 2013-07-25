f30ffdd81758a87954bc3ee7a48dbc141d674732
Shady <shady@shadyproject.net>
2013-07-25 00:59:15 -0700

    Added a one liner version

--- prediction.rb
+++ prediction.rb
@@ -2,6 +2,8 @@ def compute_prediction(n, w)
   growth_rate = 0.07
   prediction = (1..w).inject(n) {|sum, _| sum + (sum *growth_rate)}
   puts prediction.floor
+  #one liner version
+  #puts (1..w).inject(n) {|sum, _| sum + (sum*0.07)}.floor
 end
 
 uniq_per_week = 10
