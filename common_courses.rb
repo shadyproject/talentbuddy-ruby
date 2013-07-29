77a4a1a592ff7b5ac9e43c442ff2ff9c21e2d92d
Chris Martin <shady@shadyproject.net>
2013-07-29 15:28:46 -0700

    Initial implementation, not fully functional.

---
+++ common_courses.rb
@@ -0,0 +1,12 @@
+# Given two arrays that contain the course IDs of two different students
+# write a function that prints to standard output (stdout) all the course IDs
+# that are contained in both arrays, sorted in ascending order, one per line
+
+def get_common_courses(courses1, courses2)
+  puts courses1 & courses2
+end
+
+c1 = [1, 2, 8, 4, 5, 8, 3]
+c2 = [8, 2, 2, 7, 10]
+
+get_common_courses(c1, c2)
