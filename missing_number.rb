fb70349b82079caf5e69d546fdf150fa1248aa98
unknown <shady@shadyproject.net>
2013-11-08 22:23:59 -0800

    stub for missing number exercise

---
+++ missing_number.rb
@@ -0,0 +1,13 @@
+# Given an array containing all numbers from 1 to N with the exception of one print the missing number to the standard output.
+# Example input:
+# array: 5 4 1 2
+# Example output:
+# 3
+
+def find_missing_number(v)
+  puts v
+end
+
+arr = [5, 4, 1, 2]
+
+find_missing_number(arr)
